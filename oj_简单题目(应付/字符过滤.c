7265ca2549ca52099e18155c0b70c3bd3dc55b5f
calistalearns <1615699158@qq.com>
2026-01-16 15:22:39 +0800

    a

--- oj_简单题目(应付/字符过滤.c
+++ oj_简单题目(应付/字符过滤.c
@@ -1,6 +1,27 @@
-# include <stdio.h>
-int main()
-{
+#include <stdio.h>
+#include <string.h>
 
+int main() 
+{
+    char str[1000], s[100];
+    int i = 0, j = 0;
+
+    fgets(str, sizeof(str), stdin);
+    fgets(s, sizeof(s), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+    s[strcspn(s, "\n")] = '\0';
+
+    for (i = 0; str[i] != '\0'; i++) 
+    {
+        if (strchr(s, str[i]) == NULL) 
+        {
+            str[j] = str[i];
+            j++;
+        }
+    }
+    str[j] = '\0';
+
+    printf("%s", str);
     return 0;
 }
